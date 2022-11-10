9b859e3374c59ac22c3b84a8bf04d2af9a6ddb9e
IllusiveAldebaran <franciscofabiangutierrez@gmail.com>
2022-11-09 16:33:33 -0800

    new name
    new name there

--- grade.sh
+++ grade.sh
@@ -1,7 +1,17 @@
-# Create your grading script here
 
-set -e
+CP=".:lib/hamcrest-core-1.3.jar:lib/junit-4.13.2.jar"
+TESTNAME="TEST"
+STUDENT_DIR="student-dir"
 
-rm -rf student-submission
-git clone $1 student-submission
+rm -rf $STUDENT_DIR
 
+git clone $1 $STUDENT_DIR # names the repo $STUDENT_DIR
+
+cp $TESTNAME.java $STUDENT_DIR/ # the name of the test
+
+cd ./$STUDENT_DIR/
+
+
+javac -cp $CP *.java
+
+java -cp $CP org.junit.runner.JUnitCore $TESTNAME
