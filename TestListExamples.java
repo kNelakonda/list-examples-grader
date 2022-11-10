4bf170d78f2f95c98c9f4aa2e296dee7929067a2
j6villanueva <112349401+j6villanueva@users.noreply.github.com>
2022-11-09 17:24:20 -0800

    update

--- TestListExamples.java
+++ TestListExamples.java
@@ -1,6 +1,37 @@
 import static org.junit.Assert.*;
 import org.junit.*;
+import java.util.*;
 
-public class TestListExamples {
+public class TestListExamples implements StringChecker {
   // Write your grading tests here!
+  @Test
+  public void filterCorrectly() {
+    List<String> i = new ArrayList<String>();
+    List<String> endList = new ArrayList<String>();
+    i.add("1Hello");
+    i.add("We");
+    i.add("1Are");
+    i.add("Here");
+    StringChecker wc = new objChecker();
+    ListExamples.filter(i,wc);
+    endList.add("1Hello");
+    endList.add("1Are");
+    // int f = 0;
+    // while(f<i.size()){
+    //   if(wc.checkString(i.get(f))){
+    //     endList.add(i.get(f));
+    //   }
+    // }
+    assertEquals(endList,ListExamples.filter(i,wc));
+  }
+}
+class objChecker implements StringChecker{
+
+  public boolean checkString(String s){
+    if(Character.isDigit(s.charAt(0))){
+      return true;
+    }else{
+    return false;
+    }  
+  }
 }
